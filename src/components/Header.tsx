76a48237c60cc569cd36dfe60dd177da08a78823
reiend <145087085+EndreiDarkRose@users.noreply.github.com>
2024-02-16 21:55:55 +0900

    fix routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,11 @@ const Header: React.FC = () => {
       <div className="container">
         <Link to="/React-pizza">
           <div className="header__logo">
-            <img width="38" src="/assets/img/pizza-logo.svg" alt="Pizza logo" />
+            <img
+              width="38"
+              src="./assets/img/pizza-logo.svg"
+              alt="Pizza logo"
+            />
             <div>
               <h1>React Pizza</h1>
               <p>самая вкусная пицца во вселенной</p>
